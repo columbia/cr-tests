c3c12e101c9e771dc111a5b28d0c3c1303183b50
root <root@linuz11.watson.ibm.com>
2009-06-08 10:38:16 -0400

    improve simple/runtests.sh
    Don't background ckpt, test for checkpoint/restart not writing return
    value to file, and notify user of the self-checkpoint+freezer bug.
    
    Signed-off-by: Serge Hallyn <serue@us.ibm.com>

--- simple/runtests.sh
+++ simple/runtests.sh
@@ -4,24 +4,33 @@
 
 RSTR=`which rstr`
 
-./ckpt > out &
-sleep 1
+./ckpt > out
 if [ ! -f /tmp/cr-test.out ]; then
-	echo FAIL - ckpt did not create /tmp/cr-test.out
+	echo "FAIL - ckpt did not create /tmp/cr-test.out"
 	exit 1
 fi
 v=`grep ret /tmp/cr-test.out | awk -F=  '{ print $2 '}`
-if [ ! $v -gt 0 ]; then
-	echo FAIL - ckpt return value was $v, should be > 0
+if [ "x$v" == "x" ]; then
+	echo "FAIL - ckpt did not put its return value in /tmp/cr-test.out"
+	echo "Chances are sys_checkpoint() failed"
+	echo "NOTE this is a known problem at the moment - self-checkpoint"
+	echo "does not work due to a checkpoint+freezer bug"
 	exit 2
 fi
+if [ ! $v -gt 0 ]; then
+	echo "FAIL - ckpt return value was $v, should be > 0"
+	exit 3
+fi
 
-$RSTR < out &
-sleep 1
+$RSTR < out
 v=`grep ret /tmp/cr-test.out | awk -F=  '{ print $2 '}`
+if [ "x$v" == "x" ]; then
+	echo "FAIL - rstrt return value was not in /tmp/cr-test.out"
+	exit 4
+fi
 if [ $v -ne 0 ]; then
-	echo FAIL - rstrt return value was $v, should be == 0
-	exit 2
+	echo "FAIL - rstrt return value was $v, should be == 0"
+	exit 5
 fi
 echo PASS
 exit 0
