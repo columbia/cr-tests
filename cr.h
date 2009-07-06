256cc8537c42893c0c2824f6b2672d182dbcaf0a
root <root@linuz11.watson.ibm.com>
2009-07-06 15:28:32 -0400

    don't redefine __NR_{checkpoint,restart}
    Hopefully everyone has these in unistd.h, bc the #s keep changing
    so cr_tests can't really keep up.
    
    Signed-off-by: Serge Hallyn <serue@us.ibm.com>

--- cr.h
+++ cr.h
@@ -1,10 +1,27 @@
 #if __i386__
+
+#ifndef __NR_checkpoint
 #define __NR_checkpoint 335
+#endif
+#ifndef __NR_restart
 #define __NR_restart    336
+#endif
+
 #elif __s390x__
-#define __NR_checkpoint 330
-#define __NR_restart    331
+
+#ifndef __NR_checkpoint
+#define __NR_checkpoint 332
+#endif
+#ifndef __NR_restart
+#define __NR_restart    333
+#endif
+
 #elif __powerpc__
+
+#ifndef __NR_checkpoint
 #define __NR_checkpoint 322
+#endif
+#ifndef __NR_restart
 #define __NR_restart    323
+#endif
 #endif
