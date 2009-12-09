8125a58aeec8cec80bc639a01981f70d0f55d0bc
Serge E. Hallyn <serue@us.ibm.com>
2009-12-09 11:23:10 -0500

    send -1 for logfd arg to checkpoint in selinux testcase
    Signed-off-by: Serge E. Hallyn <serue@us.ibm.com>

--- selinux/ckpt.c
+++ selinux/ckpt.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	fprintf(file, "hello, world!\n");
 	fflush(file);
 
-	ret = syscall(__NR_checkpoint, pid, STDOUT_FILENO, CHECKPOINT_SUBTREE);
+	ret = syscall(__NR_checkpoint, pid, STDOUT_FILENO, CHECKPOINT_SUBTREE, -1);
 	if (ret < 0) {
 		perror("checkpoint");
 		exit(2);
