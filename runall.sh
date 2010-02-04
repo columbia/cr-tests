fb4bb777b4cf507226330fdf874a496504dccffe
Serge E. Hallyn <serue@us.ibm.com>
2010-02-04 16:27:08 -0500

    list failed jobs at end of run
    Signed-off-by: Serge E. Hallyn <serue@us.ibm.com>

--- runall.sh
+++ runall.sh
@@ -4,14 +4,20 @@
 
 declare -i total=0
 declare -i passed=0
+declare -i numfailed=0
+failed=( )
 
 # $1 = status
+# $2 testcase
 update_totals() {
     local status=$1
+    testcase=$2
     total+=1
     if [ $status -eq 0 ] ; then
 	passed+=1
     else
+	failed[$numfailed]="$testcase"
+	numfailed+=1
 	echo FAIL
     fi
 }
@@ -46,62 +52,65 @@ fi
 echo Running simple checkpoint/restart test
 pushd simple
 bash runtests.sh
-update_totals $?
+update_totals $? simple
 popd
 
 echo Running counterloop tests
 pushd counterloop
 bash runtests.sh
-update_totals $?
+update_totals $? counterloop
 popd
 
 echo Running fileio test
 pushd fileio
 bash runtests.sh
-update_totals $?
+update_totals $? fileio
 popd
 
 echo Running futex tests
 pushd futex
 bash run.sh
-update_totals $?
+update_totals $? futex
 popd
 
 echo Running restart block test
 pushd sleep
 bash runtest.sh
-update_totals $?
+update_totals $? restart
 popd
 
 echo Running process-tree tests
 pushd process-tree
 sh runtests.sh
-update_totals $?
+update_totals $? process-tree
 popd
 
 echo Running bash test
 pushd bashckpt
 bash bash-cr.sh
-update_totals $?
+update_totals $? bash
 popd
 
 echo Running ipc tests
 pushd ipc
 bash runtests.sh
-update_totals $?
+update_totals $? ipc
 popd
 
 echo Running userid/namespace test
 pushd userns
 bash runtests.sh
-update_totals $?
+update_totals $? userid
 popd
 
-echo Running cwd/chroot tests
+echo "Running taskfs (cwd/chroot) tests"
 pushd taskfs
 bash runtest.sh
-update_totals $?
+update_totals $? taskfs
 popd
 
 echo $passed out of $total test groups passed.
+echo "Failed tests:"
+for t in ${failed[@]}; do echo " $t"; done
+
 exit 0
