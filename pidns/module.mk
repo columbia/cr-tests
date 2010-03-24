7bd883e17f25d36d66f9b550194c445df2f63e7e
Serge Hallyn <serue@us.ibm.com>
2010-03-24 15:33:26 -0400

    fix up pidns make testclean rule
    Signed-off-by: Serge Hallyn <serue@us.ibm.com>

--- pidns/module.mk
+++ pidns/module.mk
@@ -2,5 +2,5 @@ local_dir := pidns
 local_progs := $(local_dir)/pidns
 
 progs += $(local_progs)
-test_clean += $(addprefix $(local_dir)/,*.o checkpoint* mypid* pidns outpid)
+test_clean += $(addprefix $(local_dir)/,*.o pidns cr_pid*)
 
