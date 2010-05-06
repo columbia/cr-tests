0ee149926c20e58f3ab1bc219c258f1714f84612
Serge Hallyn <serue@us.ibm.com>
2010-05-06 10:38:45 -0400

    use time -p
    Signed-off-by: Serge Hallyn <serue@us.ibm.com>

--- makeprocs/runtest.sh
+++ makeprocs/runtest.sh
@@ -3,7 +3,7 @@ echo "Running with $*"
 
 rm -f out out2 ready done checkpoint-done
 
-read -p "Press enter to start... " x
+#read -p "Press enter to start... " x
 nsexec -cgmpnuti -P pid ./mp $* &
 sleep 1
 pid=`cat pid`
@@ -11,13 +11,9 @@ pid=`cat pid`
 while [ ! -f "ready" ]; do : ; done
 
 echo FROZEN > /cgroup/$pid/freezer.state
-#/usr/bin/time checkpoint $pid -o out
-#/usr/bin/time checkpoint $pid -o out2
-time checkpoint $pid -o out
-time checkpoint $pid -o out2
+/usr/bin/time -p checkpoint $pid -o out
 echo THAWED > /cgroup/$pid/freezer.state
 
 touch "./checkpoint-done"
 
-#/usr/bin/time restart -i out -p
-time restart -i out -p
+/usr/bin/time -p restart -i out -p
